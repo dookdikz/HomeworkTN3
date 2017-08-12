9ae7f610456cc3fe80df43b434b2d539d10382bb
DESKTOP-84R9FGK\dookdikz <icedookdik@gmail.com>
2017-08-12 18:30:17 +0700

    create TabActivity , set logic onClick

---
+++ app/src/main/java/com/example/dookdikz/homeworktn3/TapActivity.java
@@ -0,0 +1,45 @@
+package com.example.dookdikz.homeworktn3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by dookdikz on 8/12/2017.
+ */
+
+public class TapActivity extends AppCompatActivity {
+    TextView tvCountTap;
+    ImageView ivBtnTap;
+    int number;
+    int countNumber=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap);
+        setTitle("Tapper");
+        number = getIntent().getIntExtra("number", 0);
+        initInstance();
+    }
+
+    private void initInstance() {
+        tvCountTap = (TextView) findViewById(R.id.tvCountTap);
+        ivBtnTap = (ImageView) findViewById(R.id.ivBtnTab);
+        ivBtnTap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countNumber += 1;
+                tvCountTap.setText(String.valueOf(countNumber));
+                if(countNumber>=number){
+                    Intent intent = new Intent(TapActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
